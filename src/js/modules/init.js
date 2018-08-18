030eb91fbe997e5f79816627240ee9a5a51c171c
arturios1916 <arturios1996@gmail.com>
2018-08-18 13:39:56 +0300

    add lightbox, finish galerry

--- src/js/modules/init.js
+++ src/js/modules/init.js
@@ -8,14 +8,6 @@ $(document).ready(function () {
             prevEl: '.swiper-button-prev',
         }
     });
-    $('[data-fancybox="gallery"]').fancybox({
-        beforeShow: function (instance, slide) {
-            $(".galerry__item").css("position", "relative");
-        },
-        beforeClose: function () {
-            $(".galerry__item").css("position", "absolute");
-        }
-    });
     $(".owl-carousel").owlCarousel({
         items: 7,
         loop: true,
